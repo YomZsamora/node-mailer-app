cbbe41c769cbe574bd934c635a8ecdd29f5007d8
yomZsamora <samaurah@gmail.com>
2025-04-24 05:41:01 +0300

    add exception handler import in the main application file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+const { exceptionHandler } = require('./utils/exceptions/exception-handler');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
